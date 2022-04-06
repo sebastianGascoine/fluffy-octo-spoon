b1f512ae3f8635e30f3b025995687101d8237d66
Parkat <95242011+parkat69@users.noreply.github.com>
2022-04-06 12:09:48 -0700

    cheese

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var myDatabase = require("./myDatabase");
-var routes = require("./routes");
+var myDatabase = require("./Server/myDatabase");
+var routes = require("./Server/routes");
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', express.static('./public'));
-app.use('/db', express.static('./myDatabase'));
+app.use('/', express.static('./../public'));
+app.use('/db', express.static('./Server/myDatabase'));
 
 app.use(routes);
 
