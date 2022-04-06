df57b40d442d4a2092849e4fb00286796c6448bf
notamoon <mylessamuelharris@gmail.com>
2022-04-06 10:58:02 -0700

    PIECES MOVE

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,65 @@
+const rows = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ];
+const cols = [ '8', '7', '6', '5', '4', '3', '2', '1' ];
+
 $(document).ready(function() {
-    let cell = 0;
+    let index = 0;
 
     for (let r = 0; r < 8; ++r) {
-        const row = $("#board").append('<tr></tr>');
+        const row = $('<tr></tr>').appendTo('#board');
 
         for (let r = 0; r < 8; ++r) {
-            $(row).append(`<td class="cell${cell % 2}"></td>`);
+            const cell = $(`<td class="cell"></td>`).appendTo(row);
 
-            cell++;
+            const locationRowNumber = Math.floor(index % 8);
+            const locationColNumber = Math.floor(index / 8);
+
+            const locationRow = rows[locationRowNumber];
+            const locationCol = cols[locationColNumber];
+
+            const location = locationRow + locationCol;
+
+            $(cell).prop("id", location);
+
+            $(cell).append('<span class="cell_number">' + location + '</span>');
+
+            $(cell).droppable({
+                tolerance: 'pointer',
+                drop: function(event, ui) {
+                    $(cell).css('background-color', '');
+                    $(ui.draggable).css('left', 0).css('top', 0).appendTo(cell);
+
+                    $("#moves").append('<p>' + location + '</p>')
+                },
+                over: function() {
+                    $(cell).css('background-color', 'blue');
+                },
+                out: function() {
+                    $(cell).css('background-color', '');
+                },
+                accept: function() {
+                    return !$(cell).children('.piece').length;
+                }
+            });
+
+            index++;
         }
-
-        cell++;
     }
+
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#a2');
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#b2');
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#c2');
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#d2');
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#e2');
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#f2');
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#g2');
+    $('<img src="chess_pieces/pawn.png" alt="pawn" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#h2');
+
+    $('<img src="chess_pieces/rook.png"   alt="rook"   class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#a1');
+    $('<img src="chess_pieces/knight.png" alt="knight" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#b1');
+    $('<img src="chess_pieces/bishop.png" alt="bishop" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#c1');
+    $('<img src="chess_pieces/queen.png"  alt="queen"  class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#d1');
+    $('<img src="chess_pieces/king.png"   alt="king"   class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#e1');
+    $('<img src="chess_pieces/bishop.png" alt="bishop" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#f1');
+    $('<img src="chess_pieces/knight.png" alt="knight" class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#g1');
+    $('<img src="chess_pieces/rook.png"   alt="rook"   class="piece">').draggable({ revert: 'invalid', containment: '#board' }).appendTo('#h1');
 });
