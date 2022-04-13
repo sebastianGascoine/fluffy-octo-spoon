e97cd59e27ad7188fbd55dfe45c070d310d76374
Parkat <95242011+parkat69@users.noreply.github.com>
2022-04-12 18:22:42 -0700

    linting

--- public/js/home2.js
+++ public/js/home2.js
@@ -4,25 +4,26 @@
  * and open the template in the editor.
  */
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function joinVis() {
-   // await sleep(2000);
-    $("#2").removeClass("active");//
+    // await sleep(2000);
+    $("#2").removeClass("active"); //
     $("#1").toggleClass("active");
     $("#3").removeClass("active");
 }
+
 function createVis() {
-  //  await sleep(2000);
+    //  await sleep(2000);
     $("#1").removeClass("active");
     $("#3").toggleClass("active");
-    $("#2").removeClass("active");//
+    $("#2").removeClass("active"); //
 }
+
 function fenVis() {
     //await sleep(2000);
     $("#1").removeClass("active");
     $("#2").toggleClass("active");
     $("#3").removeClass("active");
 }
-
